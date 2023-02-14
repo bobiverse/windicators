4de4e7472fee008fd95c525347ae04996867d5d9
Kārlis Gaigals <karlis@nic.lv>
2023-02-14 11:02:10 +0200

    Renamed example directory

--- examples/main.go
+++ examples/main.go
@@ -6,16 +6,16 @@ import (
 	"time"
 
 	"github.com/bobiverse/windicators"
-	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
 func main() {
 	// Define window size and position
-	iw, err := windicators.NewIndicatorWindow(200, 20, windicators.PositionCenterTop)
+	iw, err := windicators.NewIndicatorWindow(200, 20, windicators.PositionCenterBottom)
 	if err != nil {
 		log.Fatalf("new window error: %s", err)
 	}
-	defer glfw.Terminate()
+
+	defer iw.Terminate()
 
 	// First indicator component
 	windicators.NewComponent(iw, "demo-rand", "Demo(%v)", func(c *windicators.Component) any {
